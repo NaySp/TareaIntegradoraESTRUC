dd4073ffba4bd735cbf6e3463d02155b31a8a4e7
sergioflorez30 <sergiofflorez2005@gmail.com>
2023-04-19 16:30:23 -0500

    upload change in atributes class Passenger

--- src/model/Passenger.java
+++ src/model/Passenger.java
@@ -1,28 +1,39 @@
 package model;
 
 public class Passenger {
-    Boolean firstClass;
+
+    String name;
+    int id;
+    int age;
     String row;
     int numChair;
-    int priority;
+    Boolean firstClass;
+    Boolean priority;
 
-    public Passenger(Boolean firstClass, String row, int numChair, int priority) {
-        this.firstClass = firstClass;
+    int milles;
+
+    Boolean comeFirst;
+    public Passenger(String name, int id, int age, String row, int numChair, Boolean firstClass, Boolean priority, Boolean comeFirst, int mi) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
         this.row = row;
         this.numChair = numChair;
+        this.firstClass = firstClass;
         this.priority = priority;
+        this.comeFirst = comeFirst;
+        this.milles = milles;
+    }
+    public String getName() {
+        return name;
     }
 
-    public int getPriority() {
-        return priority;
+    public int getId() {
+        return id;
     }
 
-    public void setPriority(int priority) {
-        this.priority = priority;
-    }
-
-    public Boolean getFirstClass() {
-        return firstClass;
+    public int getAge() {
+        return age;
     }
 
     public String getRow() {
@@ -31,5 +42,21 @@ public class Passenger {
 
     public int getNumChair() {
         return numChair;
+    }
+
+    public Boolean getFirstClass() {
+        return firstClass;
+    }
+
+    public void setFirstClass(Boolean firstClass) {
+        this.firstClass = firstClass;
+    }
+
+    public Boolean getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Boolean priority) {
+        this.priority = priority;
     }
 }
