94914fe0551ef4d809fc6d18b375018ebf29b03a
NaySp <nsp1324@hotmail.com>
2023-04-21 15:43:15 -0500

    update, hash table R1

---
+++ src/model/IHash.java
@@ -0,0 +1,13 @@
+package model;
+
+public interface IHash<K, V> {
+
+    public void put(K key, V value);
+    public V get(K key);
+    public boolean containsKey(K key);
+    public int hash(K key);
+    public boolean isEmpty();
+    public int getSize();
+    public void resize();
+
+}
