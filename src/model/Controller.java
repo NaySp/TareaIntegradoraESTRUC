60a952948e7ef551811fa6ffc56c43bda26e870d
sergioflorez30 <sergiofflorez2005@gmail.com>
2023-04-22 15:59:35 -0500

    upload something RF2

--- src/model/Controller.java
+++ src/model/Controller.java
@@ -1,6 +1,75 @@
 
 package model;
 
+import java.util.ArrayList;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
 public class Controller {
+    
+    private HashTable<String, Passenger> hashTable;
+    private PriorityQueue<Passenger> firstClass;
+    private Queue<Passenger> turistClass;
+
+    public Controller(){
+        firstClass = new PriorityQueue<>();
+        turistClass = new Queue<>(); 
+    }
+
+    public void uploadPassengers(String archivo, int num) {
+        File file = new File(archivo);
+        hashTable = new HashTable<>(num);
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] atributs = line.split(";");
+                Passenger passenger = new Passenger(atributs[0], Integer.parseInt(atributs[1]), Integer.parseInt(atributs[2]),Integer.parseInt(atributs[3]),
+                        Integer.parseInt(atributs[4]),atributs[5],Boolean.parseBoolean(atributs[6]));
+
+                hashTable.insert(atributs[0],passenger);
+            }
+            fis.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void orderPassenger(String archivo) {
+        File file = new File(archivo);
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] atribut = line.split(" ");
+                Passenger passenger = hashTable.get(atribut[0]);
+                if (passenger != null) {
+                    passenger.toString();
+                    
+                    if (passenger.getFirstClass() == true) {
+                        firstClass.insert(passenger);
+        
+                    } else {
+                        turistClass.enqueue(passenger);
+                    }
+                }
+                else{
+                    System.out.println("no se encontro el pasajero :C");
+                }
+
+            }
+            fis.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+
+    }
+
 
 }
--- src/model/Passenger.java
+++ src/model/Passenger.java
@@ -1,6 +1,6 @@
 package model;
 
-public class Passenger {
+public class Passenger implements Comparable<Passenger> {
 
     String name;
     int id;
@@ -9,15 +9,14 @@ public class Passenger {
     String column;
     Boolean firstClass;
     int miles;
-    int comeFirst;
-    public Passenger(String name, int id, int age, int miles,int row, String column,Boolean firstClass,  int comeFirst) {
+    
+    public Passenger(String name, int id, int age, int miles,int row, String column,Boolean firstClass) {
         this.name = name;
         this.id = id;
         this.age = age;
         this.row = row;
         this.column = column;
         this.firstClass = firstClass;
-        this.comeFirst = comeFirst;
         this.miles = miles;
     }
     public String getName() {
@@ -47,5 +46,29 @@ public class Passenger {
     public void setFirstClass(Boolean firstClass) {
         this.firstClass = firstClass;
     }
+    public int getMiles() {
+        return miles;
+    }
+    public void setMiles(int miles) {
+        this.miles = miles;
+    }
+    
+    @Override
+    public String toString() {
+        return "Passenger [the Passengers name  is : " + name + "]";
+    }
+  @Override
+  public int compareTo(Passenger other){
+    int forMiles = Integer.compare(this.miles, other.getMiles());
+
+        if (forMiles != 0) {
+            // Si las millas son diferentes, se devuelve la comparación por millas
+            return forMiles;
+        } else {
+            // Si las millas son iguales, se compara por edad
+            return Integer.compare(this.age, other.getAge());
+        }
+
+  }  
 
 }
